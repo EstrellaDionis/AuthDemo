5d73b52d55e05040a1a50cc5e980076ad966deb1
EstrellaDionis <EstrellaDionis@gmail.com>
2022-05-18 19:46:11 -0400

    installed express-session to allow users to stay signed in with session id and implemented logic to wether they have access to endpoints based on their login status

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const User = require("./models/user");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const session = require("express-session");
 
 //mongo connection to db
 mongoose.connect("mongodb://localhost:27017/authDemo", {
@@ -20,6 +21,7 @@ app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: "notagoodsecret" }));
 
 app.get("/", (req, res) => {
   res.send("this is the home page");
@@ -37,8 +39,8 @@ app.post("/register", async (req, res) => {
     password: hash,
   });
   await user.save();
-  res.redirect("/");
-  res.send(hash);
+  req.session.user_id = user._id; //when you succesfully create a new user, we take the user id and store it in the session
+  res.redirect("/secret");
 });
 
 app.get("/login", (req, res) => {
@@ -47,16 +49,21 @@ app.get("/login", (req, res) => {
 
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
-  const user = await User.findOne({ username }); //we could also do username: username
-  const validPassword = await bcrypt.compare(password, user.password);
+  const user = await User.findOne({ username }); //we could also do username: username. This is finding our username in the db
+  const validPassword = await bcrypt.compare(password, user.password); //remember, .compare in bcrypt requires a plain text password as the first argument and then the hash'd password in the db as the second
   if (validPassword) {
-    res.send("Welcome!");
+    req.session.user_id = user._id; //if you do successfully login, we store the user id in the session
+    res.redirect("/secret");
   } else {
-    res.send("Try again");
+    res.send("/login");
   }
 });
 
 app.use("/secret", (req, res) => {
+  //this only works because the other routes have the user id in the session. No login, no access!
+  if (!req.session.user_id) {
+    res.redirect("/login");
+  }
   res.send("This is secret! You cannot see me unless you login");
 });
 
